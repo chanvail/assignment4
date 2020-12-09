6bce05d9bd37b782b4296ad4df826b46a3fdefce
Chantal Vaillancourt <chantal@Chantals-MacBook-Pro.local>
2020-12-09 01:03:16 -0500

    Another edit

--- js/index.js
+++ js/index.js
@@ -6,8 +6,8 @@ import aItems from "../items/index.js";
 
 class Page {
     constructor(){
-        this.sName = "Chantal Vaillancourt?";
-        this.sUrlToEmailer = "https://secret-retreat-60319.herokuapp.com/send";
+        this.sName = "Chantal Vaillancourt";
+        this.sUrlToEmailer = "";
         const sBase = document.location.pathname;
         if(sBase[sBase.length - 1] == "/"){
             this.sBase = sBase.substr(0, sBase.length -1);
