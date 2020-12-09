e7f69cc6395bf03a6830bbfcea4d671db69f9072
Chantal Vaillancourt <chantal@Chantals-MacBook-Pro.local>
2020-12-09 00:50:56 -0500

    First commit?

--- js/index.js
+++ js/index.js
@@ -6,8 +6,8 @@ import aItems from "../items/index.js";
 
 class Page {
     constructor(){
-        this.sName = "Richard Hildred";
-        this.sUrlToEmailer = "https://dry-bayou-18746.herokuapp.com/send/";
+        this.sName = "Chantal Vaillancourt";
+        this.sUrlToEmailer = "https://secret-retreat-60319.herokuapp.com/send";
         const sBase = document.location.pathname;
         if(sBase[sBase.length - 1] == "/"){
             this.sBase = sBase.substr(0, sBase.length -1);
