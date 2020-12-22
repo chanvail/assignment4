7bf26c1e40a77bcccb13d2aa3862b987ee8661d7
Chantal Vaillancourt <chantal@Chantals-MacBook-Pro.local>
2020-12-22 00:49:48 -0500

    Small edits

--- items/index.js
+++ items/index.js
@@ -1,5 +1,5 @@
-export default [{"id": "item1", "fname":"item1.md", "specialImage":"img/placeholder.png"},
-{"id": "item2", "fname":"item2.md", "specialImage":"img/placeholder.png"},
+export default [{"id": "item1", "fname":"item1.md","specialImage":"img/placeholder.png"},
+{"id": "item2", "fname":"Item1.html", "specialImage":"img/placeholder.png"},
 {"id": "item3", "fname":"item3.md", "specialImage":"img/placeholder.png"},
 {"id": "item4", "fname":"item4.md", "specialImage":"img/placeholder.png"},
 {"id": "item5", "fname":"item5.md", "specialImage":"img/placeholder.png"}];
